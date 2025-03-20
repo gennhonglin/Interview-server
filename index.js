a2448754fe8466f4075ab21199baabd9077782b8
gennhonglin <73550732+gennhonglin@users.noreply.github.com>
2025-03-19 19:49:19 -0700

    Implemented validation for phone number and email to ensure that whatever the user does on the front-end and potentially breaching the code for front-end to provide invalid information then the backend will also catch it if the front-end doesn't

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 //This is the middleware
 app.use(cors());
@@ -34,8 +35,19 @@ const FormData = mongoose.model("FormData", FormDataSchema);
 //This is the route that the front-end will send the data to
 //The data is then saved to the database and a response is sent back to the front-end
 app.post("/submit", async (req, res) => {
-    const formDetails = req.body;
+    const { fName, lName, number, email} = req.body;
 
+    //Validating the phone number
+    if(!validator.isMobilePhone(number)) {
+        return res.send("Please enter a valid phone number");
+    }
+
+    //Validating the email
+    if(!validator.isEmail(email)) {
+        return res.send("Please enter a valid email address");
+    }
+    
+    const formDetails = {fName, lName, number, email};
     const newFormDetails = new FormData(formDetails);
 
     try {
