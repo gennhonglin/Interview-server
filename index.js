820590dda89af29617c240325b4fe949a4ce6be5
gennhonglin <73550732+gennhonglin@users.noreply.github.com>
2025-03-19 18:30:50 -0700

    Implemented the back-end where database schema is developed and data from the front-end is sent and stored into mongoDB database. Should send success messages and if else sends error messages if something comes up

--- index.js
+++ index.js
@@ -1,9 +1,53 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+//This is the middleware
 app.use(cors());
 app.use(express.json());
 
 require('dotenv').config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
+
+//Connection to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log("Connected to MongoDB");
+}).catch((err) => {
+    console.log(err);
+});
+
+//Developing the Schema for the Database
+
+const FormDataSchema = new mongoose.Schema({
+    fName: String,
+    lName: String,
+    number: String,
+    email: String,
+})
+
+const FormData = mongoose.model("FormData", FormDataSchema);
+
+//This is the route that the front-end will send the data to
+//The data is then saved to the database and a response is sent back to the front-end
+app.post("/submit", async (req, res) => {
+    const formDetails = req.body;
+
+    const newFormDetails = new FormData(formDetails);
+
+    try {
+        await newFormDetails.save();
+        res.send("Data saved successfully!");
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
 
